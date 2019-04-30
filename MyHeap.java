9cb89d0d690b967f40963b1940aeed221a901b4a
Elizabeth Sham <esham00@stuy.edu>
2019-04-30 11:49:38 -0400

    not working

--- MyHeap.java
+++ MyHeap.java
@@ -30,14 +30,50 @@ public class MyHeap{
 	for(int i = parent; i >= 0; i--) {
 	    pushDown(data, data.length, i);
 	}
-
     }
     public static void heapsort(int[] data) {
 	heapify(data);
+	int swapIndex = data.length-1;
+	while (swapIndex >= 0) {
+	    swap(data, 0, swapIndex);
+	    pushDown(data, data.length, 0);
+	    swapIndex--;
+	}
     }
     public static void main(String[] args) {
 	int[] ary = new int[]{1, 2, 3, 4, 5, 6, 9, 7, 6};
-	heapify(ary);
-	System.out.print(Arrays.toString(ary));
+	heapsort(ary);
+	System.out.println(Arrays.toString(ary));
+	// int[]MAX_LIST = {1000000000,500,10};
+    	// for(int MAX : MAX_LIST){
+    	//     for(int size = 31250; size < 2000001; size*=2){
+    	// 	long qtime=0;
+    	// 	long btime=0;
+    	// 	//average of 5 sorts.
+    	// 	for(int trial = 0 ; trial <=5; trial++){
+    	// 	    int []data1 = new int[size];
+    	// 	    int []data2 = new int[size];
+    	// 	    for(int i = 0; i < data1.length; i++){
+    	// 		data1[i] = (int)(Math.random()*MAX);
+    	// 		data2[i] = data1[i];
+    	// 	    }
+    	// 	    long t1,t2;
+    	// 	    t1 = System.currentTimeMillis();
+    	// 	    heapsort(data2);
+    	// 	    t2 = System.currentTimeMillis();
+    	// 	    qtime += t2 - t1;
+    	// 	    t1 = System.currentTimeMillis();
+    	// 	    Arrays.sort(data1);
+    	// 	    t2 = System.currentTimeMillis();
+    	// 	    btime+= t2 - t1;
+    	// 	    if(!Arrays.equals(data1,data2)){
+    	// 		System.out.println("FAIL TO SORT!");
+    	// 		System.exit(0);
+    	// 	    }
+    	// 	}
+    	// 	System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    	//     }
+    	//     System.out.println();
+    	// }
     }
 }
